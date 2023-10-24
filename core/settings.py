cef99f6191578fcb2d8cfdb24fc5666722067f0c
admin <bunyod.kh@gmail.com>
2023-10-24 18:22:33 +0500

    hot fix2

--- core/settings.py
+++ core/settings.py
@@ -91,11 +91,11 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
+STATIC_URL = '/var/www/shakhrisabz/static/'
 # STATIC_ROOT = Path.joinpath(BASE_DIR, 'static')
 STATIC_ROOT = '/var/www/shakhrisabz/static'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/var/www/shakhrisabz/media/'
 # MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media')
 MEDIA_ROOT = '/var/www/shakhrisabz/media'
 
